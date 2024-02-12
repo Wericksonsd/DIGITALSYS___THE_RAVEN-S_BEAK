f7797e8f90d0564a72e700564e5dacac49a9829c
Werickson Dauta <105117577+Wericksonsd@users.noreply.github.com>
2024-02-12 16:15:23 -0300

    asasa

--- ravenbeak/src/Grupo.jsx
+++ ravenbeak/src/Grupo.jsx
@@ -19,14 +19,29 @@ function Grupo(){
     const [dinPer, setDinPer] = useState(0);
 
     return (
-        <div>
-            <img src="" alt="" />
-            <div>
-                <h2>{nome}</h2>
-                <h4>{raca}</h4>
-                <h4>{classe}</h4>
+        <div id='cardPer'>
+            <img id='imgPer' src="" alt="personagem" />
+            <div className='infosPer'>
+                <div className='infosPerNome'>
+                    <h2 id='nomePer'>{nome}</h2>
+                    <h4 id='racaPer'>{raca}</h4>
+                    <h4 id='classePer'>{classe}</h4>
+                </div>
+                <img id='origemPer' src="" alt="origem personagem"/>
+            </div>            
+            <div className='nivelPerDiv'><img src="" alt="niveis" /></div>
+            <div className='statusPer'>
+                <img src="" alt="hp" />
+                <img src="" alt="def" />
+                <img src="" alt="mana" />
             </div>
-            
+            <p id='historiaPer'></p>
+            <div className='atributosPer'>
+                <span>C</span>
+                <span>F</span>
+                <span>A</span>
+                <span>S</span>
+            </div>
         </div>
     )
 
